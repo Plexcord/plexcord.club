249d8343438112b3e1b6c0bb707efa1990124ad2
MutanPlex <barisal54@gmail.com>
2025-03-05 08:58:09 +0300

    Footer and Source Created

---
+++ src/pages/Source.jsx
@@ -0,0 +1,9 @@
+const Source = () => {
+  return (
+    <div style={{ width: "100%", maxWidth: "1200px",flex: "1", margin: "0 auto" }}>
+      <h1>Source</h1>
+    </div>
+  );
+};
+
+export default Source;
